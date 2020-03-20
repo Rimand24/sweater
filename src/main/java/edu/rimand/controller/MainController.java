2b7156678bf0d0c738ca999ac08f3fe676eab115
Rimand <Spiculum24@gmail.com>
2020-03-20 14:47:28 +1100

    controller fix

--- src/main/java/edu/rimand/controller/MainController.java
+++ src/main/java/edu/rimand/controller/MainController.java
@@ -6,6 +6,7 @@ import edu.rimand.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,35 +19,43 @@ public class MainController {
     MessageRepo messageRepo;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting(Model model) {
         return "greeting";
     }
 
     @GetMapping ("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
+
+//    private  Iterable<Message> responseFilter(String filter){
+//        Iterable<Message> messages;
+//        if (filter.equals("tagFilter")) messages = messageRepo.findByTag(filter);
+//        else if (filter.equals("authorFilter")) messages = messageRepo.findByTag(filter);
+//        else messages = messageRepo.findAll();
+//
+//        return messages;
+//    }
+
 
     @PostMapping ("/main")
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model){
+            Model model){
         Message msg = new Message(user, text, tag);
         messageRepo.save(msg);
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping ("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages = messageRepo.findByTag(filter);
-        messages = filter.isEmpty() ? messageRepo.findAll() : messageRepo.findByTag(filter);
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
         return "main";
     }
 }
