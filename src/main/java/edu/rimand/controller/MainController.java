ac45fa5f1169e1ceb54980a9e7b7abca668785ff
Rimand <Spiculum24@gmail.com>
2020-03-21 18:48:55 +1100

    +file upload

--- src/main/java/edu/rimand/controller/MainController.java
+++ src/main/java/edu/rimand/controller/MainController.java
@@ -4,19 +4,27 @@ import edu.rimand.domain.Message;
 import edu.rimand.domain.User;
 import edu.rimand.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Model model) {
@@ -41,8 +49,20 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Model model){
+            @RequestParam("file") MultipartFile file,
+            Model model
+    ) throws IOException {
         Message msg = new Message(user, text, tag);
+        if (file!=null) {
+            File dir = new File(uploadPath);
+            if (!dir.exists()){
+                dir.mkdir();
+            }
+            String uuid = UUID.randomUUID().toString();
+            String resultFilename = uuid + "." + file.getOriginalFilename();
+            msg.setFilename(resultFilename);
+            file.transferTo(new File(resultFilename));
+        }
         messageRepo.save(msg);
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
