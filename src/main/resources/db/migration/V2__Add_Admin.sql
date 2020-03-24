8f50ead8fc500dc5d36f569c4cd2314d00b3c8d3
Rimand <Spiculum24@gmail.com>
2020-03-24 14:58:42 +1100

    login form fix

--- src/main/resources/db/migration/V2__Add_Admin.sql
+++ src/main/resources/db/migration/V2__Add_Admin.sql
@@ -1,6 +1,5 @@
 insert into usr (id, username, password, active)
  values (1, 'admin', '10', true);
--- $2a$08$5Gb0G87B01Z0TVaeYU3lseU7tOMMiEhu3H9ktmQKiigvs8W/HFTDK
 
 insert into user_role (user_id, roles)
  VALUES (1, 'USER'), (1, 'ADMIN');
