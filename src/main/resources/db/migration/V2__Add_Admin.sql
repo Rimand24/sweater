5abb18bf8ca1b026d630cc03d851f89234f4e7a9
rimand24 <Spiculum24@gmail.com>
2020-05-10 18:29:16 +0300

    runnable

--- src/main/resources/db/migration/V2__Add_Admin.sql
+++ src/main/resources/db/migration/V2__Add_Admin.sql
@@ -1,5 +1,5 @@
-insert into usr (id, username, password, active)
- values (1, 'admin', '10', true);
+insert into usr (id, username, password, active, email)
+ values (1, 'admin', '123', true, 'example@mail.com'), (2, 'user', '123', true, 'example2@mail.com');
 
 insert into user_role (user_id, roles)
  VALUES (1, 'USER'), (1, 'ADMIN');
