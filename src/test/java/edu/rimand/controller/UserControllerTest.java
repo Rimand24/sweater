31a9e4e0176bf7dd00817ecfee3910e5946b7872
rimand24 <Spiculum24@gmail.com>
2020-05-20 01:18:16 +0300

    tewst

---
+++ src/test/java/edu/rimand/controller/UserControllerTest.java
@@ -0,0 +1,28 @@
+package edu.rimand.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void userlist() {
+    }
+
+    @Test
+    void userEditForm() {
+    }
+
+    @Test
+    void userSave() {
+    }
+
+    @Test
+    void getProfile() {
+    }
+
+    @Test
+    void updateProfile() {
+    }
+}
