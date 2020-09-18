bff523eaa822ffd4655c89db856c33dbeca38846
Rimand <Spiculum24@gmail.com>
2020-09-18 13:31:08 +0300

    turbolinks

--- src/main/java/edu/rimand/config/MvcConfig.java
+++ src/main/java/edu/rimand/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package edu.rimand.config;
 
+import edu.rimand.utils.RedirectIntercepter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,5 +39,10 @@ public class MvcConfig implements WebMvcConfigurer {
     private boolean isWindows(){
         String OS = System.getProperty("os.name").toLowerCase();
         return (OS.contains("win"));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectIntercepter());
     }
 }
